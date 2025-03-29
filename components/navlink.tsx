0eef35a7a8d1aab1f25314647bacf883ffa08ba9
Sreeram P <psreeram3015@proton.me>
2025-03-29 13:32:23 +0530

    Removed text-white class to be compatible in light theme

--- components/navlink.tsx
+++ components/navlink.tsx
@@ -15,7 +15,7 @@ export function NavLink({ href, children, ...props }: NavLinkProps) {
 
     return (
         <Link href={href} {...props} className={`group relative ${props.className || ""}`}>
-            <span className="font-medium text-base text-white hover:text-gray-300 transition-colors">
+            <span className="font-medium text-base">
                 {children}
             </span>
             <span
